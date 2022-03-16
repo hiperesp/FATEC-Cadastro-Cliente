1ef4de7cc9a1817092a31697e44448e84abe4248
Gabriel Lopes Ferreira Ramos <gabriel@gabstep.com.br>
2022-03-16 10:28:32 -0300

    deno init

---
+++ deno/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.get("", function(req, res) {
+  res.send("Bem-Vindo ao meu app");
+});
+
+app.get("/contato", function(req, res) {
+    res.send("Contato");
+});
+
+app.get("/produto", function(req, res) {
+    res.send("Produto");
+});
+
+app.get("/dados/:nome/:cargo", function(req, res) {
+    res.send("Olá sr(a)"+req.params.nome+", seu cargo é "+req.params.cargo);
+});
+
+app.listen(8081, function() {
+  console.log("Servidor funcionando");
+});
